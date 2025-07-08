0397972687c098a9cbcfa7f9dfefb995d485aa61
mahmoud gamal <roone858@gmail.com>
2025-07-08 17:34:38 +0300

    corsSetting

--- src/main.ts
+++ src/main.ts
@@ -6,13 +6,13 @@ import { ValidationExceptionFilter } from './common/exceptions/validation-except
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  // const corsSetting = {
-  //   origin: process.env.CLIENT_BASE_URL, // السماح فقط للطلبات القادمة من هذا الدومين
-  //   methods: 'GET,POST,PUT,DELETE', // تحديد أنواع الطلبات المسموح بها
-  //   allowedHeaders: 'Content-Type, Authorization', // تحديد الهيدرز المسموح بها
-  //   credentials: true, // السماح بإرسال الـ Cookies والجلسات
-  // };
-  app.enableCors();
+  const corsSetting = {
+    origin: process.env.CLIENT_BASE_URL, // السماح فقط للطلبات القادمة من هذا الدومين
+    methods: 'GET,POST,PUT,DELETE', // تحديد أنواع الطلبات المسموح بها
+    allowedHeaders: 'Content-Type, Authorization', // تحديد الهيدرز المسموح بها
+    credentials: true, // السماح بإرسال الـ Cookies والجلسات
+  };
+  app.enableCors(corsSetting);
   app.useGlobalPipes(
     new ValidationPipe({
       transform: true, // تحويل القيم بشكل تلقائي
